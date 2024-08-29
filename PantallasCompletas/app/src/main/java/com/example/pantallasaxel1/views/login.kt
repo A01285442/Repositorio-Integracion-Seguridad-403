6cee3d02a5ae2429d1f34a501f020bbcacad2f9b
axeliparrea <141780766+axeliparrea@users.noreply.github.com>
2024-08-28 18:46:23 -0600

    Main Correction

---
+++ PantallasCompletas/app/src/main/java/com/example/pantallasaxel1/views/login.kt
@@ -0,0 +1,111 @@
+package com.example.pantallasaxel1.views
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.pantallasaxel1.R
+import com.example.pantallasaxel1.ui.theme.BlueTEC
+
+//Login
+
+@Composable
+fun LoginScreen(navController: NavController,modifier: Modifier = Modifier) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var loginError by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "LEGAL MATCH",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = BlueTEC,
+            modifier = Modifier.padding(1.dp)
+                .padding(10.dp)
+
+        )
+
+
+
+        Image(
+            painter = painterResource(id = R.drawable.casaazul),
+            contentDescription = "LogoAxel",
+            modifier = Modifier.height(300.dp)
+
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("E-mail") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (loginError) {
+            Text(text = "Invalid credentials", color = Color.Red)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        Button(
+            onClick = {
+                navController.navigate("categories")
+
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = BlueTEC,
+                contentColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Iniciar")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "¿No tienes una cuenta?", color = Color.Black)
+        Text(text = "Registrate", color = BlueTEC)
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+
+
+    }
+
---
+++ PantallasCompletas/gradle/app/src/main/java/com/example/pantallasaxel1/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.pantallasaxel1
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.jorge1.MessagingView
+import com.example.pantallasaxel1.ui.theme.PantallasAxel1Theme
+import com.example.pantallasaxel1.views.Categories
+import com.example.pantallasaxel1.views.DetallesDeCaso
+import com.example.pantallasaxel1.views.LoginScreen
+import com.example.vistasproyecto.vistas.Casos
+
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PantallasAxel1Theme {
+                val navController = rememberNavController()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("login") {
+                            LoginScreen(navController = navController)
+                        }
+                        composable("categories") {
+                            Categories(navController = navController)
+                        }
+                        composable("casos"){
+                            Casos(navController = navController)
+                        }
+                        composable("DetallesDecaso"){
+                            DetallesDeCaso(navController = navController)
+                        }
+                        composable("MessagingView"){
+                            MessagingView(navController = navController)
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    PantallasAxel1Theme {
+        val navController = rememberNavController()
+        LoginScreen(navController = navController)
+    }
+}
