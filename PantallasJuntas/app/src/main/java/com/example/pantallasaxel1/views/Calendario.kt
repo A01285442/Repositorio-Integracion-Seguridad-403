8d2add6a7d92975ee51d147d7dab68deff28f240
axeliparrea <141780766+axeliparrea@users.noreply.github.com>
2024-09-04 13:30:39 -0600

    git commit

---
+++ PantallasJuntas/app/src/main/java/com/example/pantallasaxel1/views/Calendario.kt
@@ -0,0 +1,10 @@
+package com.example.pantallasaxel1.views//package com.example.pantallasaxel1.views
+//
+//import androidx.compose.runtime.Composable
+//
+//@Composable
+//fun CalendarioView(){
+//    val checkDate
+//
+//    var DatePick by { mutableMapOf()}
+//}
