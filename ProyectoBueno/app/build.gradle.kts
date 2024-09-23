1445a8b412de843ed9fb5e1385ca42c319fde101
fcruzl44 <ferelizondogm@gmail.com>
2024-09-23 14:28:23 -0600

    Ultimo plugin quitado

--- ProyectoBueno/app/build.gradle.kts
+++ ProyectoBueno/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     kotlin("plugin.serialization") version "2.0.0"
     alias(libs.plugins.compose.compiler)
-    kotlin("plugin.serialization") version "1.9.0"
+
 }
 
 android {
