c8da593a996d05c06a34f62ee4d1fa2ef0f3855d
axeliparrea <141780766+axeliparrea@users.noreply.github.com>
2024-10-07 15:13:52 -0600

    Grafica Edades

--- LegalMatch/app/src/main/java/com/example/legalmatch/ui/screens/Stats.kt
+++ LegalMatch/app/src/main/java/com/example/legalmatch/ui/screens/Stats.kt
@@ -2,17 +2,22 @@ package com.example.legalmatch.ui.screens
 
 import android.graphics.Color
 import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,7 +29,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -32,10 +40,12 @@ import androidx.navigation.NavController
 import com.example.legalmatch.R
 import com.example.legalmatch.ui.components.CustomBottomBar
 import com.example.legalmatch.ui.components.CustomTopBar
+import com.example.legalmatch.viewmodel.EdadStatsState
 import com.example.legalmatch.viewmodel.GraficasViewModel
 import com.example.legalmatch.viewmodel.StatsState
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
@@ -44,12 +54,14 @@ import com.github.mikephil.charting.utils.MPPointF
 import kotlinx.coroutines.delay
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun StatsScreen(navController: NavController,graficasViewModel: GraficasViewModel = viewModel()) {
     // ViewModel
     val stats by graficasViewModel.statsState.collectAsState()
     val isLoading = remember { mutableStateOf(true) }
     val errorMessage = remember { mutableStateOf<String?>(null) }
+    val edadStats by graficasViewModel.edadStatsState.collectAsState()
 
     Scaffold(
         topBar = { CustomTopBar(title = "Estadísticas", navIcon = false, actIcon = false) },
@@ -62,78 +74,208 @@ fun StatsScreen(navController: NavController,graficasViewModel: GraficasViewMode
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            when {
-                stats.isEmpty() -> {
-                    isLoading.value = true
-                    errorMessage.value = "No hay datos para mostrar"
-                    CircularProgressIndicator()
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
+                    when {
+                        stats.isEmpty() -> {
+                            isLoading.value = true
+                            errorMessage.value = "No hay datos para mostrar"
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+                        else -> {
+                            isLoading.value = false
+                            errorMessage.value = null
+                            PieChartView(stats = stats, modifier = Modifier.size(300.dp))
+                        }
+                    }
                 }
-                else -> {
-                    isLoading.value = false
-                    errorMessage.value = null
-                    PieChartView(stats = stats, modifier = Modifier.size(300.dp))
+
+                item {
+                    Spacer(modifier = Modifier.height(40.dp))
+                }
+
+                item {
+                    when {
+                        edadStats.isEmpty() -> {
+                            isLoading.value = true
+                            errorMessage.value = "No hay datos para mostrar"
+                        }
+                        else -> {
+                            isLoading.value = false
+                            errorMessage.value = null
+                            PieChartViewYears(stats = edadStats, modifier = Modifier.size(300.dp))
+                        }
+                    }
                 }
             }
+
         }
     }
 }
 
+//Genero
 @Composable
 fun PieChartView(stats: List<StatsState>, modifier: Modifier = Modifier) {
-    AndroidView(
-        modifier = modifier,
-        factory = { context ->
-            PieChart(context).apply {
-                // Configuración inicial del PieChart
-                setUsePercentValues(true)
-                description.isEnabled = false
-                setExtraOffsets(5f, 10f, 5f, 5f)
-                dragDecelerationFrictionCoef = 0.95f
-                setDrawHoleEnabled(true)
-                setHoleColor(Color.WHITE)
-                setTransparentCircleColor(Color.WHITE)
-                holeRadius = 55f
-                setDrawCenterText(true)
-                rotationAngle = 0f
-                isRotationEnabled = true
-                setHighlightPerTapEnabled(true)
-                animateY(1400, Easing.EaseInOutQuad)
-                legend.isEnabled = false
-                setEntryLabelColor(Color.BLACK)
-                setEntryLabelTextSize(12f)
-            }
-        },
-        update = { pieChart ->
-            if (stats.isNotEmpty()) {
-                val entries = stats.map { PieEntry(it.count.toFloat(), it.sexo) }
+    Column(modifier = modifier) {
+        Text(
+            text = "Género",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        )
 
-                val dataSet = PieDataSet(entries, "Usuarios por Sexo").apply {
-                    setDrawIcons(false)
-                    sliceSpace = 3f
-                    iconsOffset = MPPointF(0f, 40f)
-                    selectionShift = 5f
-                    colors = listOf(
-                        pieChart.context.getColor(R.color.purple_700),
-                        pieChart.context.getColor(R.color.red),
-                        pieChart.context.getColor(R.color.teal_200)
-                    )
+        AndroidView(
+            modifier = modifier,
+            factory = { context ->
+                PieChart(context).apply {
+                    // Configuración inicial del PieChart
+                    setUsePercentValues(true)
+                    description.isEnabled = false
+                    setExtraOffsets(5f, 10f, 5f, 5f)
+                    dragDecelerationFrictionCoef = 0.95f
+                    setDrawHoleEnabled(true)
+                    setHoleColor(Color.WHITE)
+                    setTransparentCircleColor(Color.WHITE)
+                    holeRadius = 55f
+                    setDrawCenterText(true)
+                    rotationAngle = 0f
+                    isRotationEnabled = true
+                    setHighlightPerTapEnabled(true)
+                    animateY(1400, Easing.EaseInOutQuad)
+                    setEntryLabelColor(Color.BLACK)
+                    setEntryLabelTextSize(12f)
+
+                    // Habilitar leyenda
+                    legend.isEnabled = true
+                    legend.textColor = Color.BLACK
+                    legend.textSize = 12f
+                    legend.form = Legend.LegendForm.CIRCLE
+                    legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
                 }
+            },
+            update = { pieChart ->
+                if (stats.isNotEmpty()) {
+                    val entries = stats.map { PieEntry(it.count.toFloat(), it.sexo) }
 
-                val pieData = PieData(dataSet).apply {
-                    setDrawValues(true)
-                    setValueFormatter(PercentFormatter())
-                    setValueTextSize(12f)
-                    setValueTextColor(Color.BLACK)
-                    setValueTypeface(Typeface.DEFAULT_BOLD)
+                    val dataSet = PieDataSet(entries, "").apply {
+                        setDrawIcons(false)
+                        sliceSpace = 3f
+                        iconsOffset = MPPointF(0f, 40f)
+                        selectionShift = 5f
+                        colors = listOf(
+                            pieChart.context.getColor(R.color.purple_700), // Hombre
+                            pieChart.context.getColor(R.color.red),         // Mujer
+                            pieChart.context.getColor(R.color.teal_200)     // Otro
+                        )
+                    }
+
+                    val pieData = PieData(dataSet).apply {
+                        setDrawValues(true)
+                        setValueFormatter(PercentFormatter())
+                        setValueTextSize(12f)
+                        setValueTextColor(Color.BLACK)
+                        setValueTypeface(Typeface.DEFAULT_BOLD)
+                    }
+
+                    pieChart.data = pieData
+                    pieChart.notifyDataSetChanged()
+                    pieChart.invalidate()
+                } else {
+                    pieChart.data = null
+                    pieChart.invalidate()
                 }
-
-                pieChart.data = pieData
-                pieChart.notifyDataSetChanged()
-                pieChart.invalidate()
-            } else {
-                pieChart.data = null
-                pieChart.invalidate()
             }
-        }
-    )
+        )
+    }
+}
+
+//Years
+@Composable
+fun PieChartViewYears(stats: List<EdadStatsState>, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Rangos de Edad",
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        )
+
+        AndroidView(
+            modifier = modifier,
+            factory = { context ->
+                PieChart(context).apply {
+                    setUsePercentValues(true)
+                    description.isEnabled = false
+                    setExtraOffsets(5f, 10f, 5f, 5f)
+                    dragDecelerationFrictionCoef = 0.95f
+                    setDrawHoleEnabled(true)
+                    setHoleColor(Color.WHITE)
+                    setTransparentCircleColor(Color.WHITE)
+                    holeRadius = 55f
+                    setDrawCenterText(true)
+                    rotationAngle = 0f
+                    isRotationEnabled = true
+                    setHighlightPerTapEnabled(true)
+                    animateY(1400, Easing.EaseInOutQuad)
+                    setEntryLabelColor(Color.BLACK)
+                    setEntryLabelTextSize(12f)
+
+                    // Habilitar leyenda
+                    legend.isEnabled = true
+                    legend.textColor = Color.BLACK
+                    legend.textSize = 12f
+                    legend.form = Legend.LegendForm.CIRCLE
+                    legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+                }
+            },
+            update = { pieChart ->
+                if (stats.isNotEmpty()) {
+                    val entries = stats.map { PieEntry(it.count.toFloat(), it.rangoEdad) }
+
+                    val dataSet = PieDataSet(entries, "").apply {
+                        setDrawIcons(false)
+                        sliceSpace = 3f
+                        iconsOffset = MPPointF(0f, 40f)
+                        selectionShift = 5f
+                        colors = listOf(
+                            pieChart.context.getColor(R.color.purple_700), // 18-25
+                            pieChart.context.getColor(R.color.red),         // 26-35
+                            pieChart.context.getColor(R.color.teal_200),    // 36-45
+                            pieChart.context.getColor(R.color.yellow)   // 46+
+                        )
+                    }
+
+                    val pieData = PieData(dataSet).apply {
+                        setDrawValues(true)
+                        setValueFormatter(PercentFormatter())
+                        setValueTextSize(12f)
+                        setValueTextColor(Color.BLACK)
+                        setValueTypeface(Typeface.DEFAULT_BOLD)
+                    }
+
+                    pieChart.data = pieData
+                    pieChart.notifyDataSetChanged()
+                    pieChart.invalidate()
+                } else {
+                    pieChart.data = null
+                    pieChart.invalidate()
+                }
+            }
+        )
+    }
 }
--- LegalMatch/app/src/main/java/com/example/legalmatch/viewmodel/GraficasViewModel.kt
+++ LegalMatch/app/src/main/java/com/example/legalmatch/viewmodel/GraficasViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.legalmatch.viewmodel
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,10 +11,14 @@ import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
 // Supabase
 val supabase = createSupabaseClient(
@@ -28,6 +34,7 @@ private const val TAG = "MainActivity"
 @Serializable
 data class Usuario(
     val sexo: String,
+    val fecha_nacimiento: String
 )
 
 data class StatsState(
@@ -35,13 +42,37 @@ data class StatsState(
     val count: Int
 )
 
+data class EdadStatsState(
+    val rangoEdad: String,
+    val count: Int
+)
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun calcularEdad(fechaNacimiento: String): Int {
+    val formatter = DateTimeFormatter.ISO_DATE_TIME
+    val nacimiento = LocalDate.parse(fechaNacimiento, formatter)
+    val hoy = LocalDate.now()
+    return Period.between(nacimiento, hoy).years
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 class GraficasViewModel : ViewModel() {
     private val _statsState = MutableStateFlow<List<StatsState>>(emptyList())
     val statsState: StateFlow<List<StatsState>> get() = _statsState
 
+    //Edades
+    private val _edadStatsState = MutableStateFlow<List<EdadStatsState>>(emptyList())
+    val edadStatsState: StateFlow<List<EdadStatsState>> get() = _edadStatsState
+
     init {
-        fetchSexoCounts()
+        viewModelScope.launch {
+            delay(1000)
+
+            fetchSexoCounts()
+            fetchEdadCounts()
+        }
     }
+
     fun fetchSexoCounts() {
         viewModelScope.launch {
             try {
@@ -60,4 +91,34 @@ class GraficasViewModel : ViewModel() {
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun fetchEdadCounts() {
+        viewModelScope.launch {
+            try {
+                val usuarios = supabase.from("usuarios")
+                    .select()
+                    .decodeList<Usuario>()
+
+                val edades = usuarios.map { calcularEdad(it.fecha_nacimiento) }
+                val groupedEdades = edades.groupBy { edad ->
+                    when (edad) {
+                        in 18..25 -> "18-25"
+                        in 26..35 -> "26-35"
+                        in 36..45 -> "36-45"
+                        else -> "46+"
+                    }
+                }.map { EdadStatsState(it.key, it.value.size) }
+
+                Log.d(TAG, "Edades agrupadas: $groupedEdades")
+
+                _edadStatsState.value = groupedEdades
+
+            } catch (e: Exception) {
+                Log.e(TAG, "Error fetching edad counts: ${e.message}", e)
+            }
+        }
+    }
+
+
 }
